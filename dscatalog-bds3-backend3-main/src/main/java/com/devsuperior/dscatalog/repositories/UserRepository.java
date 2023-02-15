b40ac1b20664b477ca9b25702ad762a51c3ced8c
Rafael Santos <rafa.05125@gmail.com>
2023-02-15 10:51:41 -0300

    Initial commit

---
+++ dscatalog-bds3-backend3-main/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscatalog.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
