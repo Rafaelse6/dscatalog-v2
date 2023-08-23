890d3b8e07aa5f09e604aa1e08db51b11f34a18a
Rafael Santos <rafa.05125@gmail.com>
2023-08-23 20:07:24 -0300

    Manual error handling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
 
 import './styles.css';
+import { useState } from 'react';
 
 type FormData = {
   username: string;
@@ -13,12 +14,16 @@ type FormData = {
 const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
 
+  const [hasError, setHasError] = useState(false);
+
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
+        setHasError(false);
         console.log('SUCESSO', response);
       })
       .catch((error) => {
+        setHasError(true);
         console.log('ERRO', error);
       });
   };
@@ -26,6 +31,9 @@ const Login = () => {
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
